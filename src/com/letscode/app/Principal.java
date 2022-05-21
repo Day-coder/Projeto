80d42a12fc71ba9e8fe3d174a445de48a0f60e4a
Day-coder <sistemas12day@gmail.com>
2022-05-21 11:55:29 -0300

    alterar

--- src/com/letscode/app/Principal.java
+++ src/com/letscode/app/Principal.java
@@ -10,17 +10,17 @@ import com.letscode.util.Metodos;
 public class Principal {
 	public static void main(String[] args) {
 		Leitor leitor= new Leitor();
-		//List<AtoresEAtrizes>registrosAtores= leitor.converter("D:\\Desktop\\aaa.csv");
-		List<AtoresEAtrizes>registrosAtrizes= leitor.converter("D:\\Downloads\\837c3265-e2c0-4625-b76e-da09968ccaeb.csv");// colocar o caminho do arquivo
+		List<AtoresEAtrizes>registrosAtores= leitor.converter("C:\\Users\\Dayane\\quer-ser-dev-workspace\\atores.csv");
+		List<AtoresEAtrizes>registrosAtrizes= leitor.converter("C:\\Users\\Dayane\\quer-ser-dev-workspace\\atrizes.csv");// colocar o caminho do arquivo
 		
-		//List<AtoresEAtrizes> todosAtoresEAtrizes = new ArrayList();
-		//todosAtoresEAtrizes.addAll(registrosAtores);
-		//todosAtoresEAtrizes.addAll(registrosAtrizes);
+		List<AtoresEAtrizes> todosAtoresEAtrizes = new ArrayList();
+		todosAtoresEAtrizes.addAll(registrosAtores);
+		todosAtoresEAtrizes.addAll(registrosAtrizes);
 	
 		
 		Metodos met= new Metodos();
-	met.AtorMaisJovemGanharOscar(registrosAtrizes);
-	//	met.AtrizMaisPremiada(registrosAtrizes);
+	met.AtorMaisJovemGanharOscar(registrosAtores);
+		met.AtrizMaisPremiada(registrosAtrizes);
 		met.Dados(registrosAtrizes, "Daniel Day-Lewis");
 
 	}
